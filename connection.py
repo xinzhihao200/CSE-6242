1a526f3b4e8282404117618bfedbef869e89db3c
Zhihao Xin <zhihaoxin@gatech.edu>
2017-04-22 15:20:05 -0400

    connection

--- connection.py
+++ connection.py
@@ -9,22 +9,34 @@ sys.setdefaultencoding('utf-8')
 application = Flask(__name__)
 application.debug = True
 
-
-
-
-
-
 @application.route('/welcome',method = ["GET","POST"])
+def welcome():
     if request.method == "POST":
-        string = request.form('search')
-        data = easy_search(string)
-        temp_show = {'name': 'none', 'city': 'none', 'stars': 0.0, 'address': 'none', 'categories': 'none'}
-        show_result = []
-        for element in data:
-            temp_show['name'] = element['name']
-            temp_show['categoires'] = element['categories']
-            temp_show['city'] = element['city']
-            temp_show['stars'] = element['stars']
-            temp_show['address'] = element['address']
+        r = request.form['search']
+        return redirect(url_for('search_result', request=r))
+    else:
+        return render_template('welcome.html')
 
-            show_result.append(temp_show)
+
+@application.route('/searchresult', methods=['GET','POST'])
+def search_result():
+    string = request.form['request']
+#    string = request.form('search')
+    data = easy_search(string)
+    temp_show = {'name': 'none', 'city': 'none', 'stars': 0.0, 'address': 'none', 'categories': 'none'}
+    show_result = []
+    for element in data:
+        temp_show['name'] = element['name']
+        temp_show['categoires'] = element['categories'][0]
+        temp_show['city'] = element['city']
+        temp_show['stars'] = element['stars']
+        temp_show['address'] = element['address']
+
+        show_result.append(temp_show)
+
+    return render_template('search_result.html', result=show_result)
+
+@application.route('/Restaurantpage', methods=['GET', 'POST'])
+def restaurant_page():
+    restaurant=request.form['restaurant']
+    return render_template('Restaurant_page.html', restaurant=restaurant)
